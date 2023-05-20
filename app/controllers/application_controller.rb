92437e8b45303a9736e7b741495a0ee4eac7df3d
AyakaB <ayaka.bando22@gmail.com>
2023-05-20 06:02:20 -0700

    changed nav bar for recruiters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,9 @@ class ApplicationController < ActionController::Base
         @current_user ||= User.find_by_id session[:user_id]
     end
     helper_method :current_user
+
+    def user_recruiter?
+        current_user.present? && current_user.is_recruiter?
+    end
+    helper_method :user_recruiter?
 end
