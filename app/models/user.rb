681cfa474126b67d077ab001a3c4ef426700635e
AyakaB <ayaka.bando22@gmail.com>
2023-05-22 09:54:52 -0700

    added user information

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@ class User < ApplicationRecord
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :email, presence: true, uniqueness: {message: "Email should be unique", case_sensitive: false}
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
 end
